8d276e381d964cc3c0bd3effbd6de39c38feab68
Pere <pere.solaclaver@gmail.com>
2020-04-08 10:50:07 +0100

    Add Global Style with Styled components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// GLOBAL STYLE
+import GlobalStyle from './styles/globalstyle';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <GlobalStyle />
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
