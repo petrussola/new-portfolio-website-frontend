8e3d5be5808aee98015eab304d1890fcc562b6a7
Pere <pere.solaclaver@gmail.com>
2020-04-15 13:07:12 +0100

    New favicon icon, round about me photo, hover effect on down arrow, remove commented css properties

--- src/components/Body/Projects/Projects.js
+++ src/components/Body/Projects/Projects.js
@@ -6,30 +6,18 @@ import styled from 'styled-components';
 // COMPONENTS
 import ProjectCard from './ProjectCard';
 
-// MEDIA
-import backgroundImage from '../../../media/spikes.png';
-
 const StyledSection = styled.section`
-    /* background-color: red; */
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
-    /* height: 95vh; */
-    /* margin: 0 auto; */
-    /* width: 95%; */
-    /* background-image: url(${backgroundImage}); */
-    /* border-radius: 10px;
-    box-shadow: 0.2rem 0.2rem 0.5rem gray; */
     padding-bottom: 2rem;
     div {
-        /* background-color: brown; */
         width: 100%;
         display: flex;
         flex-flow: row wrap;
         justify-content: space-evenly;
         align-items: center;
-        /* margin-top: 0; */
     }
 `;
 
